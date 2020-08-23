80ed53af6f15edf6d4aadd0c0ba7b6350f0e4b99
MelindaSW <sandstrommelinda@gmail.com>
2020-08-23 17:45:29 +0200

    initial commit

---
+++ projects.js
@@ -0,0 +1,88 @@
+export const intro = {
+  title: 'My technical portfolio',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.'
+}
+
+export const projects = [
+  {
+    name: 'LogLady',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    collaborators: [
+      { name: 'Lorem I', repo: 'link here' },
+      { name: 'Ipsum L', repo: 'link here' }
+    ],
+    images: ['ps'],
+    url: 'https://github.com'
+  },
+  {
+    name: 'Poetry Slack',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    collaborators: [
+      { name: 'Lorem I', repourl: 'link here' },
+      { name: 'Ipsum L', repourl: 'link here' }
+    ],
+    images: ['ps', 'ps'],
+    url: 'https://github.com'
+  },
+  {
+    name: 'Habit tracker',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    collaborators: [
+      { name: 'Lorem I', repo: 'link here' },
+      { name: 'Ipsum L', repo: 'link here' }
+    ],
+    images: ['ps'],
+    url: 'https://github.com'
+  },
+  {
+    name: 'DND relay writer',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    collaborators: [
+      { name: 'Lorem I', repo: 'link here' },
+      { name: 'Ipsum L', repo: 'link here' }
+    ],
+    images: ['ps'],
+    url: 'https://github.com'
+  },
+  {
+    name: 'Lorem ipsum 5',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    collaborators: [
+      { name: 'Lorem I', repourl: 'link here' },
+      { name: 'Ipsum L', repourl: 'link here' }
+    ],
+    images: ['ps', 'ps'],
+    url: 'https://github.com'
+  },
+  {
+    name: 'Lorem ipsum 4',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    collaborators: [
+      { name: 'Lorem I', repo: 'link here' },
+      { name: 'Ipsum L', repo: 'link here' }
+    ],
+    images: ['ps'],
+    url: 'https://github.com'
+  }
+]
+
+// https://github.com/wasse/slack-poem
+// https://github.com/satoskit
+// https://github.com/wasse
+
+// https://gitlab.com/JohanCederschiold/habit_tracker
+// https://gitlab.com/JensKnutson
+// https://gitlab.com/JohanCederschiold
+
+// https://github.com/MelindaSW/coffeeshop-inventory-app
+// https://github.com/MelindaSW/relay-writer
+// https://github.com/wasse/slack-poem
+// https://github.com/kits-ab/LogLady
+// https://gitlab.com/JohanCederschiold/habit_tracker
