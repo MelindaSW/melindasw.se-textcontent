ea6b05ae742a21b59cffe187ea1d1c209d0f85b5
Melinda <sandstrommelinda@gmail.com>
2020-09-09 12:21:18 +0200

    updated text content and info for new image names

--- projects.js
+++ projects.js
@@ -1,21 +1,21 @@
 export const intro = {
   title: 'My technical portfolio',
   subtitle:
-    'A selection of projects I have created on my own or contributed to.'
+    'A collection of both my own projects and others I have contributed to.'
 }
 
 export const projects = [
   {
     name: 'LogLady',
     description:
-      'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'During the spring of 2020 I did an internship course at Kits AB and got the chance to join a team working on this project. LogLady is an open source desktop application for reading log files. The major feature we developed consisted of adapting the api and the view to make the application work with larger log files without making the program slow down or using too much CPU. Other than that the sprints consisted of solving bugs and making improvements to existing features.',
     collaborators: [
       {
         name: 'Contributors',
         repourl: 'https://github.com/kits-ab/LogLady/graphs/contributors'
       }
     ],
-    images: ['ps'],
+    images: ['loglady1', 'loglady'],
     technologies: [
       'JavaScript',
       'React.js',
@@ -31,25 +31,43 @@ export const projects = [
   {
     name: 'Poetry Slack',
     description:
-      'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'This web applicaion was a school group project for the project and entreprenourship course at YRGO.It is connecting to Slacks API and to the applications backend. Based on the content of the selected channel conversation it generates different kinds of poetry. I developed the front and backend in the haiku feature, configured the backend and collaborated on the authorization solution.',
     collaborators: [
       { name: 'Satomi', repourl: 'https://github.com/satoskit' },
       { name: 'Mikel', repourl: 'https://github.com/wasse' }
     ],
-    images: ['ps', 'ps'],
-    technologies: [],
+    images: ['ps2', 'ps'],
+    technologies: [
+      'JavaScript',
+      'React.js',
+      'Java EE',
+      'TomEE (Tomcat)',
+      'REST',
+      'Maven',
+      'Swagger',
+      'Postgresql'
+    ],
     url: 'https://github.com/wasse/slack-poem'
   },
   {
     name: 'habit_tracker',
     description:
-      'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'A group project for the the server programming course at YRGO.The API creates, updates, reads and deletes users and the habits connected to the user.A user can create habits and activities for the habits.',
     collaborators: [
       { name: 'Johan', repourl: 'https://gitlab.com/JohanCederschiold' },
       { name: 'Jens', repourl: 'https://gitlab.com/JensKnutson' }
     ],
-    images: ['ps'],
-    technologies: [],
+    images: ['hbt2', 'hbt'],
+    technologies: [
+      'Java',
+      'Spring Boot',
+      'Hibernate',
+      'Postgresql',
+      'Maven',
+      'Mockito',
+      'JUnit',
+      'Swagger'
+    ],
     url: 'https://gitlab.com/JohanCederschiold/habit_tracker'
   },
 
@@ -58,21 +76,33 @@ export const projects = [
     description:
       'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     collaborators: [
-      { name: 'Lorem I', repourl: 'link here' },
-      { name: 'Ipsum L', repourl: 'link here' }
+      { name: 'Adam', repourl: 'https://gitlab.com/Hambe' },
+      { name: 'Sebastian', repourl: 'https://github.com/blueberryFields' }
     ],
-    images: ['ps', 'ps'],
-    technologies: [],
-    url: 'https://github.com'
+    images: ['cbt', 'cbt2'],
+    technologies: [
+      'Java EE',
+      'JavaScript',
+      'JBoss Wildfly',
+      'JPA',
+      'REST',
+      'Vue.js',
+      'CSS',
+      'HTML',
+      'Postgresql',
+      'Docker',
+      'Maven'
+    ],
+    url: 'https://github.com/MelindaSW/coffeeshop-inventory-app'
   },
   {
-    name: 'poetry-generator',
+    name: 'poetry-generator-api',
     description:
       'Ut enim ad minim veniam, quis nostrud exercitatio ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     collaborators: [],
-    images: ['ps'],
-    technologies: [],
-    url: 'https://github.com'
+    images: ['pg1', 'pg2'],
+    technologies: ['Java EE', 'Spring Boot', 'REST', 'Swagger', 'JUnit'],
+    url: 'https://github.com/MelindaSW/msw-poetry-generator-api'
   }
 ]
 
